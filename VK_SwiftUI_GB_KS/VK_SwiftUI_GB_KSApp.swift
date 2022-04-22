42ef3f7ef8f1f78c8f701588fdb51e8aaec6a954
KonstantinShmondrik <shmond@mail.ru>
2022-04-22 21:14:43 +0300

    Lesson-1 Вёрстка экрана логина VK

---
+++ VK_SwiftUI_GB_KS/VK_SwiftUI_GB_KSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUI_GB_KSApp.swift
+//  VK_SwiftUI_GB_KS
+//
+//  Created by Константин Шмондрик on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUI_GB_KSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
