5c52d2150a5ff2143d21c0ef9aef40b9a044947d
KonstantinShmondrik <shmond@mail.ru>
2022-05-28 20:05:38 +0300

    Lesson-8 Добавил LazyVGrid на экран с фото друзей

--- VK_SwiftUI_GB_KS/VK_SwiftUI_GB_KS/Modules/Photos/View/FriendPhotosView.swift
+++ VK_SwiftUI_GB_KS/VK_SwiftUI_GB_KS/Modules/Photos/View/FriendPhotosView.swift
@@ -13,6 +13,10 @@ struct FriendPhotosView: View {
     
     @ObservedObject var viewModel: PhotosViewModel
     
+    let columns = [
+        GridItem(.flexible(minimum: 0, maximum: .infinity)),
+        GridItem(.flexible(minimum: 0, maximum: .infinity))
+    ]
     
     init(viewModel: PhotosViewModel) {
         self.viewModel = viewModel
@@ -30,16 +34,35 @@ struct FriendPhotosView: View {
                     .lineLimit(1)
                     .font(.largeTitle)
             }
-            
-            ASCollectionView(data: viewModel.photos) {(photo, context) in
-                return PhotosViewCell(photo: photo)
-            }.layout {
-                .grid(layoutMode: .fixedNumberOfColumns(2),
-                      itemSpacing: 10,
-                      lineSpacing: 10)
+            GeometryReader { geometry in
+                ScrollView(.vertical) {
+                    LazyVGrid(columns: columns, alignment: .center, spacing: 16) {
+                        if let photos = viewModel.photos {
+                            ForEach(photos) { photo in
+                                PhotosViewCell(photo: photo)
+                                    .frame(height: geometry.size.width/2)
+                            }
+                        }
+                    }
+                }
             }
             .onAppear { viewModel.fetch() }
             .navigationBarTitle("\((viewModel.friend.firstName )) \(viewModel.friend.lastName )", displayMode: .inline)
+            
+            
+            
+            
+            
+            
+//            ASCollectionView(data: viewModel.photos) {(photo, context) in
+//                return PhotosViewCell(photo: photo)
+//            }.layout {
+//                .grid(layoutMode: .fixedNumberOfColumns(2),
+//                      itemSpacing: 10,
+//                      lineSpacing: 10)
+//            }
+//            .onAppear { viewModel.fetch() }
+//            .navigationBarTitle("\((viewModel.friend.firstName )) \(viewModel.friend.lastName )", displayMode: .inline)
         }
     }
 }
